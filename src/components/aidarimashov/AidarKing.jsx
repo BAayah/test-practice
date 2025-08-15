ca51dfa1095e04679c6c4ae1d2dc8cca4b3486f5
Aidar2025 <aidarimashovflex@gmail.com>
2025-08-15 11:02:47 +0430

    I commited bro

--- src/components/aidarimashov/AidarKing.jsx
+++ src/components/aidarimashov/AidarKing.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import img from "./../../assets/react.svg"
+import "AidarKing.css"
 
 export default function AidarKing() {
   return (
-    <div>
+    <div className='Aidar'>
         <img src={img} alt="" />
         <h1>Ассаламу алейкум World</h1>
+        <h4>I think I'm losing my mind</h4>
     </div>
   )
 }
